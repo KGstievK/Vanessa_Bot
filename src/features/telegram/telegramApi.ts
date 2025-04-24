e40c84f4370a7816a2abd6038bbbc6f045ed15c6
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2025-04-24 07:42:05 +0600

    vanessa.v4

--- src/features/telegram/telegramApi.ts
+++ src/features/telegram/telegramApi.ts
@@ -1,94 +1,94 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import {
-  ChatMember,
-  BotPermissions,
-  TelegramMessage,
-  TelegramUser,
-  TelegramApiResponse // Теперь этот тип доступен
-} from '@/types/telegram';
+  import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+  import {
+    ChatMember,
+    BotPermissions,
+    TelegramMessage,
+    TelegramUser,
+    TelegramApiResponse // Теперь этот тип доступен
+  } from '@/types/telegram';
 
-const TELEGRAM_API_TOKEN = process.env.NEXT_PUBLIC_TELEGRAM_API_TOKEN;
-if (!TELEGRAM_API_TOKEN) {
-  throw new Error('TELEGRAM_API_TOKEN is not defined');
-}
+  const TelegramToken = process.env.NEXT_PUBLIC_TELEGRAM_API_TOKEN;
+  if (!TelegramToken) {
+    throw new Error('TELEGRAM_API_TOKEN is not defined');
+  }
 
-const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}`;
+  const TELEGRAM_API_URL = `https://api.telegram.org/bot${TelegramToken}`;
 
-interface DeleteMessageParams {
-  chat_id: number;
-  message_id: number;
-}
+  interface DeleteMessageParams {
+    chat_id: number;
+    message_id: number;
+  }
 
-interface RestrictChatMemberParams {
-  chat_id: number;
-  user_id: number;
-  permissions: BotPermissions;
-  until_date?: number;
-}
+  interface RestrictChatMemberParams {
+    chat_id: number;
+    user_id: number;
+    permissions: BotPermissions;
+    until_date?: number;
+  }
 
-interface SendVideoChatParams {
-  chat_id: number;
-  title: string;
-}
+  interface SendVideoChatParams {
+    chat_id: number;
+    title: string;
+  }
 
-interface SetChatPermissionsParams {
-  chat_id: number;
-  permissions: BotPermissions;
-}
+  interface SetChatPermissionsParams {
+    chat_id: number;
+    permissions: BotPermissions;
+  }
 
-export const telegramApi = createApi({
-  reducerPath: 'telegramApi',
-  baseQuery: fetchBaseQuery({ baseUrl: TELEGRAM_API_URL }),
-  endpoints: (builder) => ({
-    deleteMessage: builder.mutation<TelegramApiResponse<boolean>, DeleteMessageParams>({
-      query: (params) => ({
-        url: '/deleteMessage',
-        method: 'POST',
-        body: params,
+  export const telegramApi = createApi({
+    reducerPath: 'telegramApi',
+    baseQuery: fetchBaseQuery({ baseUrl: TELEGRAM_API_URL }),
+    endpoints: (builder) => ({
+      deleteMessage: builder.mutation<TelegramApiResponse<boolean>, DeleteMessageParams>({
+        query: (params) => ({
+          url: '/deleteMessage',
+          method: 'POST',
+          body: params,
+        }),
+      }),
+      restrictChatMember: builder.mutation<TelegramApiResponse<boolean>, RestrictChatMemberParams>({
+        query: (params) => ({
+          url: '/restrictChatMember',
+          method: 'POST',
+          body: params,
+        }),
+      }),
+      sendVideoChat: builder.mutation<TelegramApiResponse<TelegramMessage>, SendVideoChatParams>({
+        query: (params) => ({
+          url: '/sendVideoNote',
+          method: 'POST',
+          body: params,
+        }),
+      }),
+      getChatAdministrators: builder.query<TelegramApiResponse<ChatMember[]>, number>({
+        query: (chat_id) => ({
+          url: '/getChatAdministrators',
+          method: 'POST',
+          body: { chat_id },
+        }),
+      }),
+      getMe: builder.query<TelegramApiResponse<TelegramUser>, void>({
+        query: () => ({
+          url: '/getMe',
+          method: 'POST',
+        }),
+      }),
+      setChatPermissions: builder.mutation<TelegramApiResponse<boolean>, SetChatPermissionsParams>({
+        query: (params) => ({
+          url: '/setChatPermissions',
+          method: 'POST',
+          body: params,
+        }),
       }),
     }),
-    restrictChatMember: builder.mutation<TelegramApiResponse<boolean>, RestrictChatMemberParams>({
-      query: (params) => ({
-        url: '/restrictChatMember',
-        method: 'POST',
-        body: params,
-      }),
-    }),
-    sendVideoChat: builder.mutation<TelegramApiResponse<TelegramMessage>, SendVideoChatParams>({
-      query: (params) => ({
-        url: '/sendVideoNote',
-        method: 'POST',
-        body: params,
-      }),
-    }),
-    getChatAdministrators: builder.query<TelegramApiResponse<ChatMember[]>, number>({
-      query: (chat_id) => ({
-        url: '/getChatAdministrators',
-        method: 'POST',
-        body: { chat_id },
-      }),
-    }),
-    getMe: builder.query<TelegramApiResponse<TelegramUser>, void>({
-      query: () => ({
-        url: '/getMe',
-        method: 'POST',
-      }),
-    }),
-    setChatPermissions: builder.mutation<TelegramApiResponse<boolean>, SetChatPermissionsParams>({
-      query: (params) => ({
-        url: '/setChatPermissions',
-        method: 'POST',
-        body: params,
-      }),
-    }),
-  }),
-});
+  });
 
-export const {
-  useDeleteMessageMutation,
-  useRestrictChatMemberMutation,
-  useSendVideoChatMutation,
-  useGetChatAdministratorsQuery,
-  useGetMeQuery,
-  useSetChatPermissionsMutation,
-} = telegramApi;
+  export const {
+    useDeleteMessageMutation,
+    useRestrictChatMemberMutation,
+    useSendVideoChatMutation,
+    useGetChatAdministratorsQuery,
+    useGetMeQuery,
+    useSetChatPermissionsMutation,
+  } = telegramApi;
